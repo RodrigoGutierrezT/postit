09b7cb67c7304bce3902d01bb3b3ba6ceeeb5c72
RodrigoGT <rodrigo644@gmail.com>
2023-02-19 15:50:59 -0300

    Add Logged and Login components

---
+++ app/auth/Logged.tsx
@@ -0,0 +1,30 @@
+'use client'
+
+import Image from "next/image"
+import { signOut } from 'next-auth/react'
+import Link from "next/link"
+
+type User = {
+    image: string
+}
+
+const Logged = ({image} : User) => {
+  return (
+    <li className="flex gap-8 items-center">
+        <button onClick={() => signOut()} className="bg-gray-700 text-white text-sm px-6 py-6 rounded-md">
+            Sign Out
+        </button>
+        <Link href={"/dashboard"}>
+            <Image 
+                width={64} 
+                height={64}
+                className="w-14 rounded-full" 
+                src={image} 
+                alt=""
+            />
+        </Link>
+    </li>
+  )
+}
+
+export default Logged
